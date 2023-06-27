e773337e8f658d38a5e7b48ec7cdee1cc9b35392
novus318 <nizamudheen318@gmail.com>
2023-06-27 20:19:42 +0530

    header designed

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -12,18 +12,27 @@ function Header() {
       className="shadow-sm"
       fixed="top"
     >
-      <Navbar.Brand className='ms-4' ><img className='logo' src={Logo}alt='logo'/></Navbar.Brand>
+      <Navbar.Brand className='ms-4 mb-2' ><img className='logo' src={Logo}alt='logo'/></Navbar.Brand>
       <Navbar.Toggle className='toggler' aria-controls="responsive-navbar-nav"/>
       <Navbar.Collapse className='colapsed' id="responsive-navbar-nav">
+      <div className='ms-auto ps-3'>
+      
+  <input className='p-2 search' type="text"/>
+  <i className="s fa fa-search"></i>
+
+      </div>
         <div className='ms-auto'>
         <Nav className="ms-2 me-2 ">
-          <Nav.Link>Login</Nav.Link>
-          <Dropdown><Dropdown.Toggle>afa</Dropdown.Toggle>
+          <div className='ps-3'><Nav.Link>Login</Nav.Link></div>
+          <div className='mb-2'> 
+          <Dropdown><Dropdown.Toggle>Categories</Dropdown.Toggle>
           <Dropdown.Menu>
-          <Dropdown.Item>Item</Dropdown.Item>  
-            
+          <Dropdown.Item>Item1</Dropdown.Item>
+          <Dropdown.Item>Item2</Dropdown.Item>
+          <Dropdown.Item>Item3</Dropdown.Item>
+          <Dropdown.Item>Item4</Dropdown.Item>
           </Dropdown.Menu></Dropdown>
-          <Nav.Link>Products</Nav.Link>
+          </div>
         </Nav>
         </div> 
       </Navbar.Collapse>
