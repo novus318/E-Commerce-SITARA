2ce3ca57ddb476b7a846a37fc6deea33dd1b5443
novus318 <nizamudheen318@gmail.com>
2023-06-28 16:24:06 +0530

    recomended post done

--- frontend/src/Components/Recomendation/Recomendation.js
+++ frontend/src/Components/Recomendation/Recomendation.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import Card from 'react-bootstrap/Card'
 import './Recomendation.css'
 
 function Recomendation() {
     const responsive = {
         superLargeDesktop: {
           // the naming can be any, depends on you.
-          breakpoint: { max: 3000, min: 1500 },
+          breakpoint: { max: 2000, min: 1100 },
           items: 5
         },
         desktop: {
-          breakpoint: { max: 1500, min: 800 },
+          breakpoint: { max: 1000, min: 800 },
           items: 4
         },
         tablet: {
-          breakpoint: { max: 800, min: 464 },
+          breakpoint: { max: 800, min: 400 },
           items: 2
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
+          breakpoint: { max: 400, min: 0 },
           items: 1
         }
       }
@@ -31,116 +30,92 @@ function Recomendation() {
       </div>
       <div>
       <Carousel responsive={responsive}>
-  <div className='p-card'>
-  <Card className='p-card m-auto'>
-      <Card.Img className='p-image' variant="top" src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000014745613_437Wx649H_202210030004351.jpeg" />
-      <Card.Body className='p-body'>
-        <div className='p-title'>
-            Titel
-        </div>
-        <Card.Text className='p-text row'>
-            <div className='col-8'>
-            Some quick example text to build
-            </div>
-            <div className='p-price col-4'>
-afa
-            </div>
-            </Card.Text>
-      </Card.Body>
-    </Card>
-  </div>
-  <div className='p-card'>
-  <Card className='p-card m-auto'>
-      <Card.Img className='p-image' variant="top" src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000014745613_437Wx649H_202210030004351.jpeg" />
-      <Card.Body className='p-body'>
-        <div className='p-title'>
-            Titel
-        </div>
-        <Card.Text className='p-text row'>
-            <div className='col-8'>
-            Some quick example text to build
-            </div>
-            <div className='p-price col-4'>
-afa
-            </div>
-            </Card.Text>
-      </Card.Body>
-    </Card>
-  </div>
-  <div className='p-card'>
-  <Card className='p-card m-auto'>
-      <Card.Img className='p-image' variant="top" src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000014745613_437Wx649H_202210030004351.jpeg" />
-      <Card.Body className='p-body'>
-        <div className='p-title'>
-            Titel
-        </div>
-        <Card.Text className='p-text row'>
-            <div className='col-8'>
-            Some quick example text to build
-            </div>
-            <div className='p-price col-4'>
-afa
-            </div>
-            </Card.Text>
-      </Card.Body>
-    </Card>
-  </div>
-  <div className='p-card'>
-  <Card className='p-card m-auto'>
-      <Card.Img className='p-image' variant="top" src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000014745613_437Wx649H_202210030004351.jpeg" />
-      <Card.Body className='p-body'>
-        <div className='p-title'>
-            Titel
-        </div>
-        <Card.Text className='p-text row'>
-            <div className='col-8'>
-            Some quick example text to build
-            </div>
-            <div className='p-price col-4'>
-afa
-            </div>
-            </Card.Text>
-      </Card.Body>
-    </Card>
-  </div>
-  <div className='p-card'>
-  <Card className='p-card m-auto'>
-      <Card.Img className='p-image' variant="top" src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000014745613_437Wx649H_202210030004351.jpeg" />
-      <Card.Body className='p-body'>
-        <div className='p-title'>
-            Titel
-        </div>
-        <Card.Text className='p-text row'>
-            <div className='col-8'>
-            Some quick example text to build
-            </div>
-            <div className='p-price col-4'>
-afa
-            </div>
-            </Card.Text>
-      </Card.Body>
-    </Card>
-  </div>
-  <div className='p-card'>
-  <Card className='p-card m-auto'>
-      <Card.Img className='p-image' variant="top" src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000014745613_437Wx649H_202210030004351.jpeg" />
-      <Card.Body className='p-body'>
-        <div className='p-title'>
-            Titel
-        </div>
-        <Card.Text className='p-text row'>
-            <div className='col-8'>
-            Some quick example text to build
-            </div>
-            <div className='p-price col-4'>
-afa
-            </div>
-            </Card.Text>
-      </Card.Body>
-    </Card>
-  </div>
+      <div className="product-card">
+		<div className="product-tumb">
+			<img src="https://img.tatacliq.com/images/i11/437Wx649H/MP000000017746702_437Wx649H_202305301112321.jpeg" alt=""/>
+		</div>
+		<div className="product-details">
+			<div className='p-head'>Women leather bag</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
+			<div className="product-bottom-details">
+				<div className="product-links">
+        <div className="product-price">₹ 230</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  <div className="product-card">
+		<div className="product-tumb">
+			<img src="https://img.tatacliq.com/images/i11/437Wx649H/MP000000017813986_437Wx649H_202306022359551.jpeg" alt=""/>
+		</div>
+		<div className="product-details">
+			<div className='p-head'>Women leather bag</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
+			<div className="product-bottom-details">
+				<div className="product-links">
+        <div className="product-price">₹ 230</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  <div className="product-card">
+		<div className="product-tumb">
+			<img src="https://img.tatacliq.com/images/i11/437Wx649H/MP000000017814083_437Wx649H_202306030003581.jpeg" alt=""/>
+		</div>
+		<div className="product-details">
+			<div className='p-head'>Women leather bag</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
+			<div className="product-bottom-details">
+				<div className="product-links">
+        <div className="product-price">₹ 230</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  <div className="product-card">
+		<div className="product-tumb">
+			<img src="https://img.tatacliq.com/images/i10/437Wx649H/MP000000017340053_437Wx649H_202304222223191.jpeg" alt=""/>
+		</div>
+		<div className="product-details">
+			<div className='p-head'>Women leather bag</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
+			<div className="product-bottom-details">
+				<div className="product-links">
+        <div className="product-price">₹ 230</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  <div className="product-card">
+		<div className="product-tumb">
+			<img src="https://img.tatacliq.com/images/i11/437Wx649H/MP000000017539780_437Wx649H_202305150728211.jpeg" alt=""/>
+		</div>
+		<div className="product-details">
+			<div className='p-head'>Women leather bag</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
+			<div className="product-bottom-details">
+				<div className="product-links">
+        <div className="product-price">₹ 230</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  <div className="product-card">
+		<div className="product-tumb">
+			<img src="https://img.tatacliq.com/images/i10/437Wx649H/MP000000016809635_437Wx649H_202303110201421.jpeg" alt=""/>
+		</div>
+		<div className="product-details">
+			<div className='p-head'>Women leather bag</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
+			<div className="product-bottom-details">
+				<div className="product-links">
+        <div className="product-price">₹ 230</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </Carousel>
-      </div>
+    </div>
     </div>
   )
 }
