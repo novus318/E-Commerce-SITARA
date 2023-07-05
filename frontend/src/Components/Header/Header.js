047939452899b65185750178a32c999c31f797bb
novus318 <nizamudheen318@gmail.com>
2023-07-05 16:10:42 +0530

    login and registartion

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -2,20 +2,26 @@ import './Header.css';
 import Logo from '../../assets/images/logo.png'
 import { Nav, Navbar,Dropdown} from "react-bootstrap"
 import {Link} from 'react-router-dom'
+import { MDBIcon } from 'mdb-react-ui-kit'
+import { useAuth } from '../../store/authContext';
+import toast from 'react-hot-toast';
 import { useContext } from 'react';
 import { UserContext } from '../../store/userContext';
-import { MDBIcon } from 'mdb-react-ui-kit'
-import { googleLogout } from '@react-oauth/google';
-
+import axios from 'axios';
 function Header() {
-  const {profile,setProfile} = useContext(UserContext)
-  console.log(profile)
-
-const logOut = () => {
-  googleLogout();
-  setProfile(null)
-};
-  return (
+  const {profile}=useContext(UserContext)
+  
+const[auth,setAuth]=useAuth()
+const handleLogout=()=>{
+setAuth({
+  ...auth,
+  user:null,
+  token:''
+})
+localStorage.removeItem('auth')
+toast.success('Logout Successfully')
+}
+return (
     <div className='navbar'>
     <Navbar
       collapseOnSelect
@@ -35,14 +41,14 @@ const logOut = () => {
       </div>
         <div className='ms-auto'>
         <Nav className="ms-2 me-2 ">
-          {profile? <div className='ps-3 me-3'> 
-          <Dropdown>
+        {auth.user ?<div className='ps-3 me-3'> 
+           <Dropdown>
             <Dropdown.Toggle>
           <MDBIcon className='pt-3 pb-3' icon='user-alt' size='lg' />
           </Dropdown.Toggle>
           <Dropdown.Menu>
           <Dropdown.Item className='profile' ><Link className='a-link' to='/profile'>Profile</Link></Dropdown.Item>
-          <Dropdown.Item onClick={logOut}><MDBIcon icon='sign-out-alt' size='lg'className='logout' /></Dropdown.Item>
+          <Dropdown.Item onClick={handleLogout} ><Link to='/login'><MDBIcon icon='sign-out-alt' size='lg'className='logout' /></Link></Dropdown.Item>
           </Dropdown.Menu>
           </Dropdown></div>:
           <div className='ps-3 pt-2'><Link className='a-link' to='/login'>Login</Link></div>}
